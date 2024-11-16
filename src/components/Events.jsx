ca1427ecc01af4813ed964ab1c2dcd97a7112a3d
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-11-16 12:55:28 -0600

    feat: form structure events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,10 @@
+import Form from "./Form";
+
 function Events() {
   return (
     <div className="events">
       <h2>Event Name</h2>
+      <Form />
       <p>Events go here</p>
     </div>
   );
---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import "../blocks/Form.css";
+
+function Form() {
+  return (
+    <form className="form">
+      <h2>Form</h2>
+      <input placeholder="wassup" />
+      <input />
+      <input />
+      <input />
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default Form;
