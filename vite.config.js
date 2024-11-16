29dce3dd1390b9e4dcdc06df82288ca432c9e5aa
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-11-16 12:43:57 -0600

    fix: router issue

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/code-jam_react/", // add this field
-  // base: "/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3001,
