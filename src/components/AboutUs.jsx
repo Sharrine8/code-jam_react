527805e3909e864702dbdc93d5e61c0a6f01f840
Amaterasu <darose07@yahoo.com>
2024-11-17 17:44:56 -0600

    update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,13 +1,14 @@
 import "../blocks/AboutUs.css";
 import logo from "../assets/github-logo.svg";
 import logo2 from "../assets/linkedIn-logo.svg";
+import img from "../assets/Stuart";
 
 function AboutUs() {
   return (
     <div className="about-us">
       <p>This is where we can put info about us</p>
       <div className="about-us__card">
-        <img src="" alt="" className="about-us__image" />
+        <img src={img} alt="" className="about-us__image" />
         <div className="about-us__description">
           <h1 className="about-us__title">Stuart</h1>
           <h2 className="about-us__info">okkkkkscncnscnicc</h2>
