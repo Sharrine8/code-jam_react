7dc9cda0db6e6136d17c30a8d52db8698fed96fd
Amaterasu <darose07@yahoo.com>
2024-11-17 17:02:39 -0600

    new update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,6 @@
 import "../blocks/AboutUs.css";
+import logo from "../assets/github-logo.svg";
+import logo2 from "../assets/linkedIn-logo.svg";
 
 function AboutUs() {
   return (
@@ -7,7 +9,10 @@ function AboutUs() {
       <div className="about-us__card">
         <img src="" alt="" className="about-us__image" />
         <div className="about-us__description">
-          <h2 className="about-us__title">Stuart</h2>
+          <h1 className="about-us__title">Stuart</h1>
+          <h2 className="about-us__info">okkkkkscncnscnicc</h2>
+          <button src={logo}></button>
+          <button src={logo2}></button>
         </div>
       </div>
 
