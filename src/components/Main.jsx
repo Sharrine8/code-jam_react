0edf9c6fba9e73b8646df568fb80e38f70898d25
Amaterasu <darose07@yahoo.com>
2024-11-16 18:24:05 -0600

    test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,7 +27,8 @@ function Main() {
       </section>
       <section className="main__planner">
         This is the part about the planner, with the image of a planner example
-        and a side with the info of how it works
+        and a side with the info of how it work
+        <div>okss</div>
       </section>
     </div>
   );
