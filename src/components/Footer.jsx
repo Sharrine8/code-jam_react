e219ebb735ccf77c53562db03c9d0189a558eb86
Reyes <alreyes2381@gmail.com>
2024-11-18 15:00:44 -0800

    update footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,35 @@
+import "../blocks/footer.css";
+import miniLogo from "../assets/logo-orange-mini.png";
+
 function Footer() {
   return (
-    <footer>
-      <p>Footer goes here</p>
-      <p>Video credit: @armypicca</p>
+    <footer className="footer">
+      <p className="footer__companyTag">© 2024 Company, Inc</p>
+      <img className="footer__logo" src={miniLogo} alt="logo-orange-mini" />
+      <div className="footer__container footer__border">
+        <ul className="footer__links">
+          <li>
+            <a href="../idex.html" className="footer__item">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="footer__item">
+              Creat Event
+            </a>
+          </li>
+          <li>
+            <a href="#" className="footer__item">
+              Join Event
+            </a>
+          </li>
+          <li>
+            <a href="#" className="footer__item">
+              About Us
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
