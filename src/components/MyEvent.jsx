25b38005ed32f8336c30f3592036360664970189
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-11-18 13:37:41 -0600

    add: modal markup for prep,commented out for reference

--- src/components/MyEvent.jsx
+++ src/components/MyEvent.jsx
@@ -4,12 +4,17 @@ import "../blocks/MyEvent.css";
 
 function MyEvent() {
   const [formData, setFormData] = useState(null);
+  const [dishes, setDishes] = useState([]);
   // const [isModalOpen, setIsModalOpen] = useState(false);
   // const handleOpenModal = () => {
   //   setIsModalOpen(true);
   // };
   // const handleCloseModal = () => {
   //   setIsModalOpen(false);
+  // };
+
+  // const handleAddDish = (newDish) => {
+  //   setDishes([...dishes, newDish]);
   // };
 
   useEffect(() => {
@@ -28,10 +33,26 @@ function MyEvent() {
       <h1 className="my-event__title">{formData.name}</h1>
       <p className="my-event__subtitle">Location: {formData.location}</p>
       <p className="my-event__subtitle">Date: {formData.date}</p>
-      <div className="my-event__dishes">Testing test</div>
+      <div className="my-event__dishes">
+        {dishes.length === 0 ? (
+          <p>No dishes have been added yet.</p>
+        ) : (
+          <ul>
+            {dishes.map((dish, index) => {
+              <li key={index}>{dish.name}</li>;
+            })}
+          </ul>
+        )}
+      </div>
       <button className="my-event__add-btn">Add dish</button>
+      {/* <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onSubmit={handleAddDish}
+      /> */}
     </section>
   );
 }
 
 export default MyEvent;
+//Add onClick={handleOpenModal} to button when ready
